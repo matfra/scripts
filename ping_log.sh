c00829c11e9881679c25e7e0067855018c479f73
matfra <mathieu.frappier@gmail.com>
2013-10-02 14:09:02 +0200

    Update ping_log.sh
    Remplaced , by ;
    Commented debug output

--- ping_log.sh
+++ ping_log.sh
@@ -2,23 +2,23 @@
 
 #Creating header
 echo -n "Date" > $1.csv
-while read ip ; do
-	echo -n ",$ip" >> $1.csv
-done < $1
 
+while read ip ; do
+	echo -n ";$ip" >> $1.csv
+done < $1
 
 while true ; do
 	#New line
 	echo "" >> $1.csv
 	date=`date -u "+%d/%m/%Y %H:%M"`
 	echo -n "$date" >> $1.csv
-	echo $date
+	#echo $date
 	
 	while read ip ; do
 		#New line
 		ping=`ping -c1 -q $ip |grep "=" |cut -d "/" -f4`
-		echo -n ",$ping" >> $1.csv
-		echo "IP : $ip / $ping"
+		echo -n ";$ping" >> $1.csv
+		#echo "IP : $ip / $ping"
 	done < $1
 sleep 55
 
