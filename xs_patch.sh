0f7a5a9f3d23815fd6634fabfaa9b61dad71023b
matfra <mathieu.frappier@gmail.com>
2014-06-04 10:30:42 -0700

    Update xs_patch.sh
    Added tmp dir

--- xs_patch.sh
+++ xs_patch.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-LOCAL_UUID="08112a18-4fc6-44b4-9e17-172c94a20e2d"
+LOCAL_UUID="08112a18-4fc6-44b4-9e17-XXXXXXXXXXXX"
 PATCH_DIR=/root/updates
-PATCH_DONE_DIR=/root/updates/done
+PATCH_TMP_DIR=/root/updates/tmp
+PATCH_DONE_DIR=/root/updates/archives
 
 if [ $# -eq 0 ]
   then
   echo "Please, give some Patch numbers as arguement :
-  Example $0 CTX140052"
+  Example $0 CTX140052 CTX140015 CTX140089"
   exit 1
 fi
 
@@ -14,8 +15,11 @@ fi
 for var in "$@"
 do
   PATCH_URL=`curl --silent http://support.citrix.com/article/$var |awk -F "\"" '/<a href.*\.zip/ {print $2}' |uniq`
-  echo "downloading patch at $PATCH_URL"
-  cd $PATCH_DIR
+  echo "Downloading patch at $PATCH_URL"
+  mkdir $PATCH_DIR 2>/dev/null
+  mkdir $PATCH_TMP_DIR 2>/dev/null
+  mkdir $PATCH_DONE_DIR 2>/dev/null
+  cd $PATCH_TMP_DIR
   wget $PATCH_URL
   PATCH_FILE=`basename $PATCH_URL`
   unzip $PATCH_FILE
@@ -25,6 +29,5 @@ do
   echo "Patch UUID : $UUID"
   echo "Installing the patch $PATCH"
   xe patch-apply host-uuid=$LOCAL_UUID uuid=$UUID
-  mkdir $PATCH_DONE_DIR 2>/dev/null
-  rm -i $PATCH_NAME*
+  rm -rf $PATCH_TMP_DIR
 done
