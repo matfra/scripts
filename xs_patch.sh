e4f32d15966e7a3ccb0b5d6dc9d086755f703e2d
matfra <mathieu.frappier@gmail.com>
2014-07-18 11:35:28 -0700

    Patch UUID OK
    Updates bug with patch UUID

--- xs_patch.sh
+++ xs_patch.sh
@@ -2,6 +2,7 @@
 PATCH_DIR=/root/updates
 PATCH_TMP_DIR=/root/updates/tmp
 PATCH_DONE_DIR=/root/updates/archives
+LOGFILE=$PATCH_TMP_DIR/patch.log
 
 if [ $# -eq 0 ]
   then
@@ -12,19 +13,25 @@ fi
 
 LOCAL_UUID=`xe host-list |awk '/uuid/ {print $5}'`
 echo "Patch will be applied on this machine : $LOCAL_UUID"
+echo "You have 5 sec to hit CTRL + C"
+sleep 5
 
 for var in "$@"
 do
   PATCH_URL=`curl --silent http://support.citrix.com/article/$var |awk -F "\"" '/<a href.*\.zip/ {print $2}' |uniq`
+  PATCH_FILE=`basename $PATCH_URL`
+  PATCH_NAME=${PATCH_FILE%.*}
   echo "Downloading patch at $PATCH_URL"
   mkdir $PATCH_DIR 2>/dev/null
   mkdir $PATCH_TMP_DIR 2>/dev/null
   mkdir $PATCH_DONE_DIR 2>/dev/null
-  wget -P $PATCH_DONE_DIR $PATCH_URL
-  PATCH_FILE=`basename $PATCH_URL`
+  if test -f $PATCH_DONE_DIR/$PATCH_FILE ; then
+    echo "Patch already downloaded, trying to apply it"
+  else
+    wget -P $PATCH_DONE_DIR $PATCH_URL
+  fi
   unzip $PATCH_DONE_DIR/$PATCH_FILE -d $PATCH_TMP_DIR
-  PATCH_NAME=${PATCH_FILE%.*}
-  UUID=`xe patch-upload file-name=$PATCH_TMP_DIR/$PATCH_NAME.xsupdate 2>&1 |awk '/uuid/ {print $2}'`
+  UUID=`xe patch-upload file-name=$PATCH_TMP_DIR/$PATCH_NAME.xsupdate`
   echo "Patch UUID : $UUID"
   echo "Installing the patch $PATCH_NAME"
   xe patch-apply host-uuid=$LOCAL_UUID uuid=$UUID
